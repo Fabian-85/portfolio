4f7c02af73d3a7b2b0e54edecb680c17a8a9f270
Fabian-85 <fabian.maier0805@hotmail.com>
2025-03-05 22:32:21 +0100

    above the folld section animation (button and headline)

--- src/app/mainpage/above-the-fold/above-the-fold.component.ts
+++ src/app/mainpage/above-the-fold/above-the-fold.component.ts
@@ -8,12 +8,79 @@ import { CommonModule } from '@angular/common';
   styleUrl: './above-the-fold.component.scss',
 })
 export class AboveTheFoldComponent {
-  
-  navigateToContactSection(){
-      const element = document.getElementById('contact');
-      if (element) {
-        element.scrollIntoView();
-      }
-    
+  helloWorldText = 'Hello World';
+  hover = false;
+  fix = false;
+  timeoutId: any;
+  firstWord = 'Frontend';
+  secondWord = 'Entwickler';
+  firstWordAsArray: any = [];
+  secondWordAsArray: any = [];
+
+  ngOnInit() {
+    this.firstWordAsArray = this.firstWord.split('').map((char) => ({
+      original: char,
+      display: char,
+      changeLetter:false
+    }));
+
+    this.secondWordAsArray = this.secondWord.split('').map((char) => ({
+      original: char,
+      display: char,
+      changeLetter:false
+    }));
+  }
+
+  changeLetterOnFirstWorld(index: any) {
+    this.firstWordAsArray[index].changeLetter=true;
+    this.firstWordAsArray[index].display =
+      this.firstWordAsArray[index].original ==
+      this.firstWordAsArray[index].original.toUpperCase()
+        ? this.firstWordAsArray[index].original.toLowerCase()
+        : this.firstWordAsArray[index].original.toUpperCase();
+  }
+
+  resetLetterOnFirstWorld(index: any) {
+    this.firstWordAsArray[index].changeLetter=false;
+    this.firstWordAsArray[index].display =
+      this.firstWordAsArray[index].original;
+  }
+
+  changeLetterOnSecondWorld(index: any) {
+    this.secondWordAsArray[index].changeLetter=true;
+    this.secondWordAsArray[index].display =
+      this.secondWordAsArray[index].original ==
+      this.secondWordAsArray[index].original.toUpperCase()
+        ? this.secondWordAsArray[index].original.toLowerCase()
+        : this.secondWordAsArray[index].original.toUpperCase();
+  }
+
+  resetLetterOnSecondWorld(index: any) {
+    this.secondWordAsArray[index].changeLetter=false;
+    this.secondWordAsArray[index].display =
+      this.secondWordAsArray[index].original;
+  }
+
+  showButtonAnimation() {
+    this.hover = true;
+    this.timeoutId = setTimeout(() => {
+      this.helloWorldText = "I'm Fabian Maier-Kumpukkattu";
+      this.fix = true;
+      this.hover = false;
+    }, 800);
+  }
+
+  resetButtonAnimation() {
+    clearTimeout(this.timeoutId);
+    this.hover = false;
+    this.fix = false;
+    this.helloWorldText = 'Hello World';
+  }
+
+  navigateToContactSection() {
+    const element = document.getElementById('contact');
+    if (element) {
+      element.scrollIntoView();
+    }
   }
 }
