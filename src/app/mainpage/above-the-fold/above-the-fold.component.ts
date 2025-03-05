b2840617fcd2138116fcfc98e5ffba7f2a838a18
Fabian-85 <fabian.maier0805@hotmail.com>
2025-03-05 18:45:13 +0100

    design above the fold section

--- src/app/mainpage/above-the-fold/above-the-fold.component.ts
+++ src/app/mainpage/above-the-fold/above-the-fold.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-above-the-fold',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './above-the-fold.component.html',
-  styleUrl: './above-the-fold.component.scss'
+  styleUrl: './above-the-fold.component.scss',
 })
 export class AboveTheFoldComponent {
-
+  
+  navigateToContactSection(){
+      const element = document.getElementById('contact');
+      if (element) {
+        element.scrollIntoView();
+      }
+    
+  }
 }
