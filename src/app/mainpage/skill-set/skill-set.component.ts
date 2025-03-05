8d785d22b30f2c72a6a7adc35360eea12fb442d0
Fabian-85 <fabian.maier0805@hotmail.com>
2025-03-05 12:30:00 +0100

    add skill section

--- src/app/mainpage/skill-set/skill-set.component.ts
+++ src/app/mainpage/skill-set/skill-set.component.ts
@@ -5,8 +5,19 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './skill-set.component.html',
-  styleUrl: './skill-set.component.scss'
+  styleUrl: './skill-set.component.scss',
 })
 export class SkillSetComponent {
-
+  skillsIcons = [
+    { name: 'HTML', icon: 'html.png' },
+    { name: 'CSS', icon: 'css.png' },
+    { name: 'JavaScript', icon: 'javaScript.png' },
+    { name: 'TypeScript', icon: 'typeScript.png' },
+    { name: 'Angular', icon: 'angular.png' },
+    { name: 'Firebase', icon: 'firebase.png' },
+    { name: 'Git', icon: 'git.png' },
+    { name: 'Rest API', icon: 'api.png' },
+    { name: 'Scrum', icon: 'scrum.png' },
+    { name: 'Material Design', icon: 'material-design.png' },
+  ];
 }
