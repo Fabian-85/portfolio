e0add3c9773fcd189296cc8d550e5284e3a00a8d
Fabian-85 <fabian.maier0805@hotmail.com>
2025-03-13 16:48:35 +0100

    works on about section

--- src/app/mainpage/about-me/about-me.component.ts
+++ src/app/mainpage/about-me/about-me.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrl: './about-me.component.scss',
 })
 export class AboutMeComponent {
+  isHeadlineDecorationVisible = false;
 
+  /*
+  intervalId:any;
+  ngOnInit(): void {
+    this.intervalId=setInterval(() => {
+      this.isHeadlineDecorationVisible = !this.isHeadlineDecorationVisible;
+      console.log('dd');
+    }, 3000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }*/
 }
