d99ce7f05ae538719e53991261c5bcdbdd893170
Fabian-85 <fabian.maier0805@hotmail.com>
2025-03-15 16:57:01 +0100

    works on contact section (design and responsive)

--- src/app/mainpage/contact-formular/contact-formular.component.ts
+++ src/app/mainpage/contact-formular/contact-formular.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-contact-formular',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, CommonModule],
   templateUrl: './contact-formular.component.html',
-  styleUrl: './contact-formular.component.scss'
+  styleUrl: './contact-formular.component.scss',
 })
 export class ContactFormularComponent {
+  hoverOnFirstInput = false;
+  hoverOnSecondInput = false;
+  hoverOnTextField = false;
 
+  highlightOnFirstInput() {
+    this.hoverOnFirstInput = true;
+  }
+
+  removeHighlightingFromFirstInput() {
+    this.hoverOnFirstInput = false;
+  }
+
+  highlightOnSecondInput() {
+    this.hoverOnSecondInput = true;
+  }
+
+  removeHighlightingFromSecondInput() {
+    this.hoverOnSecondInput = false;
+  }
+
+  highlightOnTextfield() {
+    this.hoverOnTextField = true;
+  }
+
+  removeHighlightingFromTextfield() {
+    this.hoverOnTextField = false;
+  }
 }
